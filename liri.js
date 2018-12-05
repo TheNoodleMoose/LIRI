ff02e9a753815735ad9e185447f5cd9db562700e
TheNoodleMoose <cjh605@gmail.com>
2018-12-05 13:57:14 -0500

    Updated the logic to run into a function

--- liri.js
+++ liri.js
@@ -17,163 +17,97 @@ let command = process.argv[2];
 
 let arguments = process.argv;
 
-let searchTerms = "";
+let searchTerms = process.argv.splice(3).join(" ");
 
-if (command == "spotify-this-song") {
-    for (i = 3; i < arguments.length; i++) {
-        searchTerms += arguments[i] + " ";
-    }
-    spotify
-        .search({ type: 'track', query: searchTerms })
-        .then(function (response) {
-            // console.log(response.tracks)
-            for (i = 0; i < response.tracks.items.length; i++) {
+
+
+function runCommand(command, searchTerms) {
+
+    if (command == "spotify-this-song") {
+        spotify
+            .search({ type: 'track', query: searchTerms })
+            .then(function (response) {
+                // console.log(response.tracks)
+
                 console.log("=====================")
-                console.log(`Name Of Artists: ${response.tracks.items[i].artists[0].name}`)
-                console.log(`Name Of Song: ${response.tracks.items[i].name}`)
-                console.log(`Preview URL: ${response.tracks.items[i].preview_url}`)
-                console.log(`From The Album: ${response.tracks.items[i].album.name}`)
+                console.log(`Name Of Artists: ${response.tracks.items[0].artists[0].name}`)
+                console.log(`Name Of Song: ${response.tracks.items[0].name}`)
+                console.log(`Preview URL: ${response.tracks.items[0].preview_url}`)
+                console.log(`From The Album: ${response.tracks.items[0].album.name}`)
                 console.log("=====================")
-            }
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-} else if (command == "movie-this") {
-    for (i = 3; i < arguments.length; i++) {
-        searchTerms += arguments[i];
-    }
-    if(searchTerms != null) {
-        searchTerms = "Mr.Nobody"
-    }
-    axios.get("https://www.omdbapi.com/?t=" + searchTerms + "&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-            console.log("======================");
-            console.log(`Title Of Movie: ${response.data.Title}`);
-            console.log(`Year Released: ${response.data.Year}`);
-            console.log(`imdb Rating: ${response.data.imdbRating}`);
-            console.log(`Rotten Tomoatoes Score: ${response.data.Ratings[1].Value}`);
-            console.log(`Country Filmed In: ${response.data.Country}`);
-            console.log(`Languages Of Film: ${response.data.Languages}`);
-            console.log(`Plot Of Film: ${response.data.Plot}`);
-            console.log(`Actors: ${response.data.Actors}`);
-            console.log("======================");
-        })
 
-} else if (command == "concert-this") {
-    for (i = 3; i < arguments.length; i++) {
-        searchTerms += arguments[i];
-    }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    } else if (command == "movie-this") {
 
-    axios.get("https://rest.bandsintown.com/artists/" + searchTerms + "/events?app_id=codingbootcamp").then(
-        function (response) {
-            // If the axios was successful...
-            // Then log the body from the site!
-            for (i = 0; i < response.data.length; i++) {
-                console.log("=========================")
-                console.log(`Artist Lineup: ${response.data[i].lineup.join(", ")}`)
-                console.log(`Venue Name: ${response.data[i].venue.name}`)
-                console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}`);
-                console.log("Date Of Concert: " + moment(response.data[i].datetime).format("MM/DD/YYYY"))
-                console.log("=========================")
-            }
-        },
-
-        function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        })
-
-} else if (command == "do-what-it-says") {
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
+        console.log(searchTerms)
+        if (searchTerms == null) {
+            searchTerms = "Mr.Nobody"
         }
+        axios.get("https://www.omdbapi.com/?t=" + searchTerms + "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                console.log("======================");
+                console.log(`Title Of Movie: ${response.data.Title}`);
+                console.log(`Year Released: ${response.data.Year}`);
+                console.log(`imdb Rating: ${response.data.imdbRating}`);
+                console.log(`Rotten Tomoatoes Score: ${response.data.Ratings[1].Value}`);
+                console.log(`Country Filmed In: ${response.data.Country}`);
+                console.log(`Languages Of Film: ${response.data.Languages}`);
+                console.log(`Plot Of Film: ${response.data.Plot}`);
+                console.log(`Actors: ${response.data.Actors}`);
+                console.log("======================");
+            })
 
-        var dataArr = data.split(",");
-        // If the text in random.txt is spotify-this-song run spotify search
-        if (dataArr[0] == "spotify-this-song") {
-            spotify
-                .search({ type: 'track', query: dataArr[1] })
-                .then(function (response) {
-                    // console.log(response.tracks)
-                    for (i = 0; i < response.tracks.items.length; i++) {
-                        console.log("=====================")
-                        console.log(`Name Of Artists: ${response.tracks.items[i].artists[0].name}`)
-                        console.log(`Name Of Song: ${response.tracks.items[i].name}`)
-                        console.log(`Preview URL: ${response.tracks.items[i].preview_url}`)
-                        console.log(`From The Album: ${response.tracks.items[i].album.name}`)
-                        console.log("=====================")
-                    }
-                })
-        } else if (dataArr[0] == "concert-this") {
-            axios.get("https://rest.bandsintown.com/artists/" + dataArr[1] + "/events?app_id=codingbootcamp").then(
-                function (response) {
-                    // If the axios was successful...
-                    // Then log the body from the site!
-                    for (i = 0; i < response.data.length; i++) {
-                        console.log("=========================")
-                        console.log(`Artist Lineup: ${response.data[i].lineup.join(", ")}`)
-                        console.log(`Venue Name: ${response.data[i].venue.name}`)
-                        console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}`);
-                        console.log("Date Of Concert: " + moment(response.data[i].datetime).format("MM/DD/YYYY"))
-                        console.log("=========================")
-                    }
-                },
+    } else if (command == "concert-this") {
 
-                function (error) {
-                    if (error.response) {
-                        // The request was made and the server responded with a status code
-                        // that falls out of the range of 2xx
-                        console.log(error.response.data);
-                        console.log(error.response.status);
-                        console.log(error.response.headers);
-                    } else if (error.request) {
-                        // The request was made but no response was received
-                        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                        console.log(error.request);
-                    } else {
-                        // Something happened in setting up the request that triggered an Error
-                        console.log("Error", error.message);
-                    }
-                    console.log(error.config);
-                })
+        axios.get("https://rest.bandsintown.com/artists/" + searchTerms + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                // If the axios was successful...
+                // Then log the body from the site!
+                for (i = 0; i < response.data.length; i++) {
+                    console.log("=========================")
+                    console.log(`Artist Lineup: ${response.data[i].lineup.join(", ")}`)
+                    console.log(`Venue Name: ${response.data[i].venue.name}`)
+                    console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}`);
+                    console.log("Date Of Concert: " + moment(response.data[i].datetime).format("MM/DD/YYYY"))
+                    console.log("=========================")
+                }
+            },
 
-        } else if (dataArr[0] == "movie-this") {
-            axios.get("https://www.omdbapi.com/?t=" + dataArr[1] + "&y=&plot=short&apikey=trilogy").then(
-                function (response) {
-                    console.log("======================");
-                    console.log(`Title Of Movie: ${response.data.Title}`);
-                    console.log(`Year Released: ${response.data.Year}`);
-                    console.log(`imdb Rating: ${response.data.imdbRating}`);
-                    console.log(`Country Filmed In: ${response.data.Country}`);
-                    console.log(`Languages Of Film: ${response.data.Languages}`);
-                    console.log(`Plot Of Film: ${response.data.Plot}`);
-                    console.log(`Actors: ${response.data.Actors}`);
-                    console.log(`Metascore: ${response.data.Metascore}`);
-                    console.log("======================");
-                })
-        }
-        else {
-            console.log("Command Not Found")
-        }
+            function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            })
 
-
-
-    })
-} else {
-    console.log("COMMAND NOT FOUND")
+    } else if (command == "do-what-it-says") {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            var dataArr = data.split(",");
+            // If the text in random.txt is spotify-this-song run spotify search
+            command = dataArr[0];
+            searchTerms = dataArr[1];
+            runCommand(command, searchTerms);
+        })
+    } else {
+        console.log("COMMAND NOT FOUND")
+    }
 }
+
+runCommand(command, searchTerms)
