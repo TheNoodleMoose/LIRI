83978e7d3ecc80defd3da1b07cf26c2aaab32d4a
TheNoodleMoose <cjh605@gmail.com>
2018-12-05 20:38:56 -0500

    Added comments to liri.js

--- liri.js
+++ liri.js
@@ -1,28 +1,27 @@
 
+//Initializes dotenv and requires it
 let dotenv = require("dotenv").config();
-
+//Initializes keys.js and requires it
 let keys = require("./keys.js");
-
+//Initializes fs and requires it
 let fs = require("fs");
-
+//Initializes moment and requires it
 let moment = require("moment");
-
+//Initializes spotify api and requires it
 let Spotify = require('node-spotify-api');
-
+//Initializes the key codes for spotify and requires it
 let spotify = new Spotify(keys.spotify);
-
+//Initializes axios and requires it
 let axios = require("axios");
-
+//Sets the second index of process.argv to be the command
 let command = process.argv[2];
-
-let arguments = process.argv;
-
+//Sets searchTerm to be equal to the spliced and rejoined string of what the user types in
 let searchTerms = process.argv.splice(3).join(" ");
 
-
-
+//This is the function that runs the whole liri bot
+//It takes in the arguments of command and  searchTerms
 function runCommand(command, searchTerms) {
-
+    //THIS RUNS THE SPOTIFY-THIS-SONG COMMAND
     if (command == "spotify-this-song") {
         spotify
             .search({ type: 'track', query: searchTerms })
@@ -40,9 +39,9 @@ function runCommand(command, searchTerms) {
             .catch(function (err) {
                 console.log(err);
             });
+    //THIS RUNS THE MOVIE-THIS COMMAND
     } else if (command == "movie-this") {
-
-        console.log(searchTerms)
+        //This checks if searchTerms is null than set searchTerms to Mr.Nobody
         if (searchTerms == null) {
             searchTerms = "Mr.Nobody"
         }
@@ -59,13 +58,12 @@ function runCommand(command, searchTerms) {
                 console.log(`Actors: ${response.data.Actors}`);
                 console.log("======================");
             })
-
+    //THIS RUNS THE CONCERT-THIS COMMAND
     } else if (command == "concert-this") {
 
         axios.get("https://rest.bandsintown.com/artists/" + searchTerms + "/events?app_id=codingbootcamp").then(
             function (response) {
                 // If the axios was successful...
-                // Then log the body from the site!
                 for (i = 0; i < response.data.length; i++) {
                     console.log("=========================")
                     console.log(`Artist Lineup: ${response.data[i].lineup.join(", ")}`)
@@ -75,11 +73,9 @@ function runCommand(command, searchTerms) {
                     console.log("=========================")
                 }
             },
-
+            //If there is an error run this..
             function (error) {
                 if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
                     console.log(error.response.data);
                     console.log(error.response.status);
                     console.log(error.response.headers);
@@ -93,24 +89,27 @@ function runCommand(command, searchTerms) {
                 }
                 console.log(error.config);
             })
-
+    //THIS RUNS THE DO-WHAT-IT-SAYS COMMAND
     } else if (command == "do-what-it-says") {
+        //This reads the random.txt file
         fs.readFile("random.txt", "utf8", function (error, data) {
             if (error) {
                 return console.log(error);
             }
             
-            // If the text in random.txt is spotify-this-song run spotify search
+            //This function will grab a random command from random.txt and run it
             function grabCommand() {
-
+                //This grabs a random number between 0 and 5 and stores it in index
                 let index = Math.floor(Math.random() * 6);
-            
+                //If index is even  than set command to index and searchTerms to index + 1
+                //Then run the runCommand Function
                 if(index % 2 == 0) { 
                     var dataArr = data.split(",");
                     command = dataArr[index];
                     searchTerms = dataArr[(index + 1)];
                     runCommand(command, searchTerms);
                 } else {
+                    //If the index is off than run again until it grabs a even number
                     grabCommand();
                 }
             }
@@ -118,9 +117,10 @@ function runCommand(command, searchTerms) {
             
             
         })
+    //If the command is not one of the options than console log this
     } else {
         console.log("COMMAND NOT FOUND")
     }
 }
-
+//Run the runCommand function right from the start
 runCommand(command, searchTerms)
